1a0af0404b6b0a5a19f32476cf1b5ce9ac694118
Devon Stanton <devon.stanton@gmail.com>
2022-07-19 08:59:23 +0200

    working featch on html data

--- webcrawler.js
+++ webcrawler.js
@@ -1,11 +1,22 @@
 import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
 
 export const getWebAddress = (url) => {
     console.log(`fetching url ${url}`)
 
     fetch(url)
     .then(response => {
-        console.log(response.text)
+        if(!response.ok){
+            throw error
+        }
+
+        return response.text();
+
+    }).then(data => {
+       // select elements from data
+       const $ = cheerio.load(data);
+       console.log($.html())
+       
     })
 
 }
