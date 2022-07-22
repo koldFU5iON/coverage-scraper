101ebf2cd7607f4f83d2ac39eea33425b247cb55
Devon Stanton <devon.stanton@gmail.com>
2022-07-22 12:16:19 +0200

    installing express and index.js code

--- src/airtable.js
+++ src/airtable.js
@@ -1,34 +1,36 @@
-import Airtable from 'airtable';
-import 'dotenv/config';
+import Airtable from "airtable";
+import "dotenv/config";
 
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appl2a4TBuQImJIiu');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  "appl2a4TBuQImJIiu"
+);
 
 // let recordID = 'recxZLSGzpE43yvdq' // test record
-const tableID = 'tblMXiuJrT9lvXo6U';
+const tableID = "tblMXiuJrT9lvXo6U";
 const table = base(tableID);
 
 export const findRecord = async (recordID) => {
-    try {
-        return await table.find(recordID)
-    } catch(err) {
-        console.log(`${err.message} : ${err.error}`)
-    }
-} 
+  try {
+    return await table.find(recordID);
+  } catch (err) {
+    console.log(`${err.message} : ${err.error}`);
+  }
+};
 
 // update record with ID
 export const updateRecord = async (recordID, data) => {
-            table.update(recordID, data, {typecast: true}, (err, record) => {
-                if(!err) {
-                    console.log(`Record was updated successfully : ${record.getId()}`) //.get('Project Name')
-                } else {
-                    console.error(`${err.message} : ${err.error}`);
-                }
-            })
-        }
+  table.update(recordID, data, { typecast: true }, (err, record) => {
+    if (!err) {
+      console.log(`Record was updated successfully : ${record.getId()}`); //.get('Project Name')
+    } else {
+      console.error(`${err.message} : ${err.error}`);
+    }
+  });
+};
 
-export const createField = async(recordID, data, field) => {
-    table.update(recordID, 'Genre',  )
-}
+export const createField = async (recordID, data, field) => {
+  table.update(recordID, "Genre");
+};
 
 // {
 //     "id": "rectL3Mp4Dg6MX2y8",
@@ -39,5 +41,5 @@ export const createField = async(recordID, data, field) => {
 // }
 
 export const updateRecords = async (records) => {
-    table.update(records)
-}
+  table.update(records);
+};
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { steamGame } from "./main.js";
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json())
+
+app.listen(
+    PORT, 
+    () => {console.log(`server running on http://localhost:${PORT}`)
+})
+
+app.get('/steam-game/:id',(req,res) => {
+    const { id } = req.params
+    if(!id) {
+        res.status(200).send(`could not read ID : ${id}`)
+    } else{
+        res.status(200).send(`successfully recieved ${id}`)
+        steamGame(id)
+    }
+})
+
+
+// todo 
+app.get('/nintendo-game/:id', (req, res) => {
+    const { id } = req.params
+})
