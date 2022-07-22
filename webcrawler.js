144e7086e4b8bba97622e9e7951cf9cd553d706e
Devon Stanton <devon.stanton@gmail.com>
2022-07-22 12:02:16 +0200

    update before git upload

--- webcrawler.js
+++ webcrawler.js
@@ -5,13 +5,13 @@ const _findDate = () => {
   // find date
 };
 
-const searchQuery = async (element, html) => {
+const searchQuery = async (tag, html) => {
   const dom = new JSDOM(html).window.document;
 
   try {
-    return dom.querySelector(element).textContent.trim();
+    return dom.querySelector(tag).textContent.trim();
   } catch (e) {
-    console.error(`Error reading contents of ${element} : ${e.message}`);
+    console.error(`Error reading contents of ${tag} : ${e.message}`);
   }
 };
 
@@ -26,14 +26,15 @@ const fetchArticle = async (url) => {
 };
 
 export const getWebAddress = async (url, elements) => {
-  elements.hostname = new URL(url).hostname;
 
   const html = await fetchArticle(url);
+  elements.url = url;
+  elements.hostname = new URL(url).hostname;
 
   elements.title = await searchQuery("h1", html);
   elements.date = await searchQuery("time", html);
 
   console.log(
-    `\n###\n${elements.hostname}\nTitle of page is: \n${elements.title}\nIt was publised on: ${elements.date}`
+    `\n###\n${elements.hostname}\nTitle of page is: \n${elements.title}\nIt was publised on: ${elements.date}\nURL: ${elements.url}`
   );
 };
